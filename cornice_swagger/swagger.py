0df02db776c6ca81885b677f8f2a2f1e7d2daa44
Gabriela Surita <gabsurita@gmail.com>
2017-02-13 13:29:18 -0200

    remove generate_swagger_spec call

--- cornice_swagger/swagger.py
+++ cornice_swagger/swagger.py
@@ -612,21 +612,3 @@ class CorniceSwagger(object):
         for transformer in self.schema_transformers:
             schema = transformer(schema, args)
         return schema
-
-
-def generate_swagger_spec(services, title, version, **kwargs):
-    """Utility to turn cornice web services into a Swagger-readable file."""
-
-    message = ("`generate_swagger_spec` is deprecated, `CorniceSwagger` class instead")
-    warnings.warn(message, DeprecationWarning)
-
-    def get_tags_from_path(service, method):
-        return [service.path.split("/")[1]]
-
-    swag = CorniceSwagger(services, def_ref_depth=-1, param_ref=0)
-    doc = swag(title, version, ignores=('head'),
-               default_tags=get_tags_from_path,
-               summary_docstrings=True, **kwargs)
-    doc.update(**kwargs)
-
-    return doc
